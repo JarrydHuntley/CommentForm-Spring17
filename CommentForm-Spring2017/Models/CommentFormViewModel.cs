f7a8d8268b1340310578e5567df210cc2d9e0448
Jarryd Huntley <Jarryd Huntley>
2017-05-23 15:05:31 -0400

    Added in the commentform viewmodel.

---
+++ CommentForm-Spring2017/Models/CommentFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommentForm_Spring2017.Models
+{
+    public class CommentFormViewModel
+    {
+        public Comment comment { get; set; }
+        public Procedure procedure { get; set; }
+    }
+}
