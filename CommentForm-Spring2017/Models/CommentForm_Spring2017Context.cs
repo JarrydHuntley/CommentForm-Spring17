5d4678d02f69a07f821dd3341cf80694c9c6b3a4
Jarryd Huntley <Jarryd Huntley>
2017-05-23 14:32:52 -0400

    Added in models, controllers and views.

---
+++ CommentForm-Spring2017/Models/CommentForm_Spring2017Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CommentForm_Spring2017.Models
+{
+    public class CommentForm_Spring2017Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CommentForm_Spring2017Context() : base("name=CommentForm_Spring2017Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CommentForm_Spring2017.Models.Comment> Comments { get; set; }
+
+        public System.Data.Entity.DbSet<CommentForm_Spring2017.Models.Procedure> Procedures { get; set; }
+    }
+}
---
+++ CommentForm-Spring2017/Models/Procedure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CommentForm_Spring2017.Models
+{
+    public class Procedure
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public int Priority { get; set; }
+    }
+}
